654894f917b4a039bf28c63c4c23d380d8c9e315
g15914km <g15914km@s30431.tsuda.ac.jp>
2017-05-19 11:52:39 +0900

    変更しました

--- src/toEnglish/IntToEng.java
+++ src/toEnglish/IntToEng.java
@@ -8,16 +8,26 @@ public class IntToEng {
 
 	        Scanner sc = new Scanner(System.in);
 	        int input = sc.nextInt();
-
+	        translateEng(input);	
 	        System.out.println(translateEng(input));
 
 	    }
 
 	    // 数値を英訳する変換するメソッド
 	    static String translateEng(int n) {
-	    	/*int i = 0;
-	    	String s = String.valueOf(i);
-	    	System.out.println(s);*/
-	        return "";
+	    	String eng = "";
+	    	switch (n) {
+			case 0: break;
+			case 1: eng  = "one"; break;
+			case 2: eng = "two"; break;
+			case 3: eng = "three"; break;
+			case 4: eng = "four"; break;
+			case 5: eng = "five"; break;
+			case 6: eng = "six"; break;
+			case 7: eng = "seven"; break;
+			case 8: eng = "eight"; break;
+			case 9: eng = "nine"; break;
+	    	}
+	    return eng;
 	    }
 	}
